50cc7ea8eb2a915fff588f01992fd54374ce1094
ElecRui <1216150778@qq.com>
2024-02-08 21:50:45 +0800

    master commit2

--- src/main/java/elecRui/Main.java
+++ src/main/java/elecRui/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.printf("Hello and welcome!2");
         System.out.printf("Hello and welcome!3");
         System.out.printf("Hello and welcome!4");
+        System.out.printf("master test");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
